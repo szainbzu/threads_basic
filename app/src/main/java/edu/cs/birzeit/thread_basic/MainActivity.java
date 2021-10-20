a2d3d7a684f09d90f04f68f788ed9b7669bb6353
szainbzu <szain@birzeit.edu>
2021-10-20 17:26:32 +0200

    Version1

--- app/src/main/java/edu/cs/birzeit/thread_basic/MainActivity.java
+++ app/src/main/java/edu/cs/birzeit/thread_basic/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
